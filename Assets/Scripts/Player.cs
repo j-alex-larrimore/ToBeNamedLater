177d69931bd155ba89360eddfeb915859e5f9380
Alex Larrimore <j.alex.larrimore@gmail.com>
2016-07-07 15:06:54 -0700

    Added horizontal movement using the rigidBody.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	private Rigidbody2D rigidBody;
 	public float speed;
-
-	private bool grounded = false;
 
 	// Use this for initialization
 	private void Start () {
-
+		rigidBody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +18,9 @@ public class Player : MonoBehaviour {
 
 	// FixedUpdate is called once per physics cycle
 	private void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
 
+		rigidBody.AddForce (new Vector2 (moveHorizontal*speed, 0));
 	}
 
 }
