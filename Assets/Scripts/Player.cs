4a61a7a16de03252c9e2c0b7af2484d92a4a15f6
Alex Larrimore <j.alex.larrimore@gmail.com>
2016-07-07 13:35:01 -0700

    Added code to Player script to take user Attack input and launch attack animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public float jumpForce;
+	private Rigidbody2D rigidBody;
+	private Animator animator;
+
+	private bool grounded = false;
+
+	// Use this for initialization
+	private void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		// || means OR
+		if(Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+			//Does this code if the statement is true
+			Jump ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Attack ();
+		}
+	}
+
+	// FixedUpdate is called once per physics cycle
+	private void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		if (moveHorizontal != 0) {
+			animator.SetBool ("playerWalking", true);
+		} else {
+			animator.SetBool ("playerWalking", false);
+		}
+
+		rigidBody.velocity = new Vector2 (moveHorizontal*speed, rigidBody.velocity.y);
+
+
+		Vector3 groundCheck = transform.position;
+		groundCheck.y -= 1;
+		grounded = Physics2D.Linecast (transform.position, groundCheck, 1 << 
+			LayerMask.NameToLayer ("Ground"));
+	}
+
+	private void Jump(){
+		if(grounded){
+			rigidBody.AddForce(new Vector2(0f, jumpForce));
+		}
+	}
+
+	private void Attack(){
+		animator.SetTrigger ("playerAttack");
+	}
+
+}
